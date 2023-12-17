ffd0e15f253ae177db1c8b4dbecba5a42ca85dac
BD-Arfat <arfat1islam5162@gmail.com>
2023-12-17 14:52:49 +0600

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 const app: Application = express();
-const port = 3000;
+import cors from "cors";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.get("/", (req:Request, res:Response) => {
+  res.send("Working successfully!");
 });
 
 export default app;
